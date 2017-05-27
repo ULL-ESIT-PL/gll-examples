f3806b77388f4b66ced519f8af0b50022c2f33f0
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-05-27 09:35:45 +0100

    reccalc.js

--- reccalc.js
+++ reccalc.js
@@ -7,28 +7,34 @@ var alt = $p.alt;
 var regex = $p.regex;
 var {setWhites, token} = require("token");
 
+/* Lexical analysis */
 var NUMBER = token(/\d+/); 
 var LP = token('(');
 var RP = token(')');
 var MULT = token(new RegExp('[*/]'));
 var ADD = token(/[+-]/);
 
+/* Recursive rules */
+var Exp = () => exp;
+var Term = () => term;
+
+/* Grammar */
 var factor = alt(
   NUMBER.map((m) => parseInt(m)),
-  seq( LP, () => e, RP).map(m => m[1])
+  seq( LP, Exp, RP).map(m => m[1])
 );
 var term = alt(
-  $p([ () => term, MULT, factor ]).map(([t, op, f]) => eval(t+op+f)),
+  $p([ Term, MULT, factor ]).map(([t, op, f]) => eval(t+op+f)),
   factor
 );
-var e = alt(
-  $p([ () => e, ADD, term ]).map(([t, op, f]) => eval(t+op+f)),
+var exp = alt(
+  $p([ Exp, ADD, term ]).map(([t, op, f]) => eval(t+op+f)),
   term
 );
 
 
 var input = "3 + 5 * (4 - 2)";
 console.log(input);
-r = e.run(input);
-console.log(ins(r)); 
+r = exp.run(input);
+console.log(ins(r));  // 13
 
